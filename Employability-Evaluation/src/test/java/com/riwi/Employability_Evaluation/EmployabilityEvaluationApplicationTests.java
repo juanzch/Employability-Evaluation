4e5aad766701d576499520330d98ed8f342cc7d7
juanzch <juanmacardenas.co@gmail.com>
2024-07-29 08:52:18 -0500

    Add files via upload

---
+++ Employability-Evaluation/src/test/java/com/riwi/Employability_Evaluation/EmployabilityEvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.Employability_Evaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployabilityEvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
